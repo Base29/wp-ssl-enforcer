5436279bc9d14720e09155086a5d9cce4bd9ca1e
Omer Hussain <omer@base29.com>
2015-07-30 18:27:37 +0500

    Update wp_ssl_enforcer.php

--- wp_ssl_enforcer.php
+++ wp_ssl_enforcer.php
@@ -14,13 +14,19 @@
 add_action('init', 'wse_force_https');
 
 function wse_force_https(){
-
-    
     if(!is_ssl()){
         wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
         exit();
     }
 }
+
+// registering setting
+add_action('admin_init', 'wse_reg_settings');
+
+function wse_reg_settings() {
+    register_settings('wse_settings', 'wse_settings');
+}
+
 
 add_action('admin_menu', 'wse_settings_page');
 
@@ -30,17 +36,11 @@ function wse_settings_page(){
 
 function wse_settings(){
     
+    $wse_setting = esc_attr( get_option('some_other_option') );
+    
 $checkBox = $_POST['adminssl'];
-$sbmt = $_POST['sbmt'];
-    
-    
-        if(isset($checkBox)){
-            $checked = "checked";
-        } else {
-            $checked = "unchecked";
-        }
         
-        if($checked == 'checked'){
+        if($wse_setting == 1){
             force_ssl_admin(TRUE);
             echo 'SSL enforced on Admin';
         } else {
@@ -50,10 +50,11 @@ $sbmt = $_POST['sbmt'];
     ?>
     <div class="wrap">
      
-        <form method="post" action="">
+        <form method="post" action="options.php">
+            <?php setting_fields('wse_settings'); ?>
             <table>
                 <tr>
-                    <td><input type="checkbox" value="<?php echo $checked; ?>" name="adminssl" <?php echo $checked; ?>/></td>
+                    <td><input type="checkbox" value="1" name="adminssl" <?php checked($wse_setting, 1); ?> /></td>
                     <td><span>Enforce SSL in admin</span></td>
             </tr>
             <tr>
