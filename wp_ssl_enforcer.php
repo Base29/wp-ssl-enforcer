6ecf7c8c75eb0bad7dd13fab1c66ef60431ba5af
Base29 <faisal@base29.com>
2015-12-14 15:11:53 +0500

    code overhaul

--- wp_ssl_enforcer.php
+++ wp_ssl_enforcer.php
@@ -6,60 +6,89 @@
  * Author: Base29
  * Author URI: http://www.base29.com
  * Version: 1.0
+ * Text Domain: wp-ssl-enforcer
+ * Domain Path: /languages
  */
 
-
-
-add_action('init', 'wse_force_https');
-
-function wse_force_https() {
-    if (!is_ssl()) {
-        wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
-        exit();
+class WSE {
+    
+    function __construct(){
+        add_action('admin_menu', array($this, 'wse_admin_menu'));
+        add_action('admin_init', array($this, 'wse_settings'));
+        add_action('init', array($this, 'wse_force_https'));
+        add_action('admin_init', array($this, 'wse_force_admin_https'));
     }
-}
-
-// registering setting
-add_action('admin_init', 'wse_reg_settings');
-
-function wse_reg_settings() {
-    register_setting('wse_settings', 'wse_settings');
-}
-
-add_action('admin_menu', 'wse_settings_page');
-
-function wse_settings_page() {
-    add_menu_page('WordPress SSL Enforcer', 'WP SSL Enforcer', 'manage_options', 'wse-ssl', 'wse_settings');
-}
-
-function wse_settings() {
-
-    $wse_setting = esc_attr(get_option('wse_settings'));
-
-    $checkBox = $_POST['adminssl'];
-
-    if ($wse_setting == 1) {
-        force_ssl_admin(TRUE);
-        echo '<div id="message" class="updated" style="margin-left:0;"><p>SSL enforced on Admin is enabled</p></div>';
-    } else {
-        echo '<div id="message" class="updated" style="margin-left:0;"><p>SSL enforcement on admin is disabled</p></div>';
+    
+    function wse_settings(){
+        register_setting('wse_plugin_settings', 'wse_domain_to_force_https');
+        register_setting('wse_plugin_settings', 'wse_force_https_on_admin');
     }
-    ?>
-    <div class="wrap">
+    
+    // Add admin menu
+    function wse_admin_menu() {
+        add_submenu_page('options-general.php', 'WP SSL Enforcer Settings', 'WP SSL Enforcer', 'manage_options', 'wse_settings', array($this, 'wse_settings_page'));
+    }
+    
+        // Settings page
+    function wse_settings_page() {
 
-        <form method="post" action="options.php">
-            <?php settings_fields('wse_settings'); ?>
-            <table>
+        echo '<div class="wrap">';
+        echo '<h1>' . get_admin_page_title() . '</h1>';
+        ?>
+        <form action="options.php" method="post">
+
+            <?php settings_fields('wse_plugin_settings'); ?>
+            <table width="50%" border="0" cellspacing="0" cellpadding="0" style="margin-top: 18px;">
+                <?php if(is_multisite()): ?>
                 <tr>
-                    <td><input type="checkbox" value="1" name="wse_settings" <?php checked($wse_setting, 1); ?> /></td>
-                    <td><span>Enforce SSL in admin</span></td>
+                    <td><h4><label><?php _e('Domain Name', 'wp-ssl-enforcer') ?></label></h4></td>
+                    <td><input type="text" name="wse_domain_to_force_https" value="<?php echo get_option('wse_domain_to_force_https') ?>"></td>
                 </tr>
+                <?php endif ?>
+                
                 <tr>
-                    <td><input type="submit" class="button-primary" value="Save Settings" name="sbmt" /></td>
+                    <td style="padding:0 0 30px"><h4><label><?php _e('Force SSL on Admin', 'wp-ssl-enforcer') ?></label></h4></td>
+                    <td style="padding:0 0 30px"><input type="checkbox" value="1" <?php checked(1, get_option('wse_force_https_on_admin'), TRUE); ?> name="wse_force_https_on_admin" ></td>
+                </tr>
+                
+                <tr>
+                    <td><input type="submit" value="<?php _e('Save Changes', 'wp-ssl-enforcer'); ?>" name="submit" id="submit" class="button button-primary"></td>
                 </tr>
             </table>
         </form>
-    </div>
+<?php echo get_option('wse_force_https_on_admin'); ?>
+        <?php
+        echo '</div>';
+    }
+    
+    function wse_force_https(){
+        
+        if(is_multisite()){
+        
+            $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            $domain = get_option('wse_domain_to_force_https');
 
-    <?php
+            if(!is_ssl() && strpos($url, $domain)){
+                wp_redirect('https://' . $url, 301);
+                exit();
+            }
+        } else {
+            if(!is_ssl()){
+                wp_redirect('https://' . $_SERVER['HTTP_POST'] . $_SERVER['REQUEST_URI'], 301);
+                exit();
+            }
+        }
+
+    }
+    
+    function wse_force_admin_https(){
+        $c = get_option('wse_force_https_on_admin');
+        if($c == 1){
+            force_ssl_admin(TRUE);
+        } else {
+            force_ssl_admin(FALSE);
+        }
+    }
 }
+
+new WSE;
